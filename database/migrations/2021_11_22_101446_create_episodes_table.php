6ed9847c437867204d1c533aeece7341e0530c8b
BahaStriker <bahastriker@outlook.com>
2021-11-22 11:24:46 +0100

    episodes table

---
+++ database/migrations/2021_11_22_101446_create_episodes_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateEpisodesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('episodes', function (Blueprint $table) {
+            $table->id();
+            $table->string('title');
+            $table->string('server_name');
+            $table->string('server_link');
+            $table->string('link');
+            $table->integer('order');
+            $table->integer('anime_id');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('episodes');
+    }
+}
