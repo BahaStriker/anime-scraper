100341fb24eb982325442980fc81465e8ab9fb3d
BahaStriker <bahastriker@outlook.com>
2021-11-22 12:30:08 +0100

    episodes test

--- app/Http/Controllers/API/MALController.php
+++ app/Http/Controllers/API/MALController.php
@@ -24,34 +24,26 @@ class MALController extends Controller
     {
         // $html = (string)file_get_contents('https://kissanimefree.to/anime-list/');
         // $crawler = new Crawler($html);
+        $test = "/category/hackgu-returner";
+        $exploded = explode('/', $test);
+        $url = 'https://gogoanime.cm/'. end($exploded). '-episode-1';
 
         // $crawler->filter('body > div.wrapper > div.main-content.mt-3 > div.container > div.row.mt-3 > aside.col-xs-12.col-lg-8 > section > div.body > ul.anime-list-v');
-        $crawler = $this->client->request('GET', 'https://gogoanime.cm/category/hackgu-returner');
+        $crawler = $this->client->request('GET', $url);
         //dd($crawler);
 
         //$crawler->filter('body > div.wrapper > div.main-content.mt-3 > div.container > div.row.mt-3 > aside.col-xs-12.col-lg-8 > section > div.body > ul.anime-list-v > li > div.info > a')->each(function ($node) {
         //$crawler->filter('body > div#body > div.container > aside.main > section > div.body > ul.anime-list-v > li > div.info > a[href]')->each(function ($node) {
-        // $crawler->filter('body > div#wrapper_inside > div#wrapper > div#wrapper_bg > section.content > section.content_left > div.main_body > div.anime_list_body > ul.listing > li > a')->each(function ($node) {
-        //     $this->list .= $node->text() . '|' . $node->attr('href') . '</br>';
-        // });
-        $hello = array('hi');
-        $img = $crawler->filter('body > div#wrapper_inside > div#wrapper > div#wrapper_bg > section.content > section.content_left > div.main_body > div.anime_info_body > div.anime_info_body_bg > img')->attr('src');
-        $episodes = $crawler->filter('body > div#wrapper_inside > div#wrapper > div#wrapper_bg > section.content > section.content_left > div.main_body > div.anime_video_body > ul#episode_page > li > a')->attr('ep_end');
-        $type = $crawler->filter('body > div#wrapper_inside > div#wrapper > div#wrapper_bg > section.content > section.content_left > div.main_body > div.anime_info_body > div.anime_info_body_bg > p.type > a')->text();
-        $desc = $crawler->filter('body > div#wrapper_inside > div#wrapper > div#wrapper_bg > section.content > section.content_left > div.main_body > div.anime_info_body > div.anime_info_body_bg > p.type')->each(function ($node) {
-            array_push($this->hello, $node->text());
+        $crawler->filter('body > div#wrapper_inside > div#wrapper > div#wrapper_bg > section.content > section.content_left > div.main_body > div.anime_video_body > div.anime_muti_link > ul > li > a')->each(function ($node) {
+            $this->list .= str_replace('Choose this server', '', $node->text()) . ' | ' . $node->attr('data-video') . '</br>';
         });
-        $this->list = $img . ' | ' . $type;
-        $array = array();
-
-        foreach($this->hello as $hello)
-        {
-            $key = strtok($hello, ':');
-            $array[$key] = substr($hello, strpos($hello, ":") + 2);
-            //dd(substr($hello, strpos($hello, ":") + 2));
-        }
-
-        dd($episodes);
+        // $hello = array('hi');
+        // $img = $crawler->filter('body > div#wrapper_inside > div#wrapper > div#wrapper_bg > section.content > section.content_left > div.main_body > div.anime_info_body > div.anime_info_body_bg > img')->attr('src');
+        // $episodes = $crawler->filter('body > div#wrapper_inside > div#wrapper > div#wrapper_bg > section.content > section.content_left > div.main_body > div.anime_video_body > ul#episode_page > li > a')->attr('ep_end');
+        // $type = $crawler->filter('body > div#wrapper_inside > div#wrapper > div#wrapper_bg > section.content > section.content_left > div.main_body > div.anime_info_body > div.anime_info_body_bg > p.type > a')->text();
+        // $desc = $crawler->filter('body > div#wrapper_inside > div#wrapper > div#wrapper_bg > section.content > section.content_left > div.main_body > div.anime_info_body > div.anime_info_body_bg > p.type')->each(function ($node) {
+        //     array_push($this->hello, $node->text());
+        // });
 
         // $crawler->filter('body > div#body > div.container > aside.main > section > div.body > ul.anime-list-v > li > div.info')->each(function ($node) {
         //     $url = $node->filter('a');
